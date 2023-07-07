1f1b24f9697a3cca45db33a75e88bdd51d813f3c
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-07 18:43:50 -0400

    added a button and input

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  const [newItemName, setNewItemName] = useState("");
+
   return (
-    <div>Form</div>
+    <form onSubmit={}>
+      <h4>grocery bud</h4>
+      <div className='form-control'>
+        <input type="text"  className='form-imput' value={newItemName} onChange={(event)=> setNewItemName(event.target.value) }/>
+        <button type='submit' className='btn'></button>
+      </div>
+
+    </form>
   )
 }
 
