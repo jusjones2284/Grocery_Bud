be6b083fb28651ef6b07a5bd034ef5b26372fd9b
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-07 20:29:15 -0400

    added inline

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 
 const SingleItem = ({item, removeItems }) => {
   const [isChecked, setIsChecked] = useState(item.completed)
-  
+
   return (
     <div className='single-item'>
-      <input type='checkbox'/>
-      <p>{item.name}</p>
+      <input type='checkbox' checked={isChecked} onChange={setIsChecked(!isChecked)}/>
+      <p style={{textTransform: 'capitalize', textDecoration: isChecked && 'line-through'}}>{item.name}</p>
       <button className='btn remove-btn' type='button'> delete </button>
 
     </div>
